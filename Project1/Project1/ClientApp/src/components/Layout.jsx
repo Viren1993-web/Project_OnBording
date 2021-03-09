143f550fe605d4c5ff4914f113f30d6f758f3aa9
Viren1993-web <75879651+Viren1993-web@users.noreply.github.com>
2021-03-10 06:59:27 +1300

    Navigation

--- Project1/Project1/ClientApp/src/components/Layout.jsx
+++ Project1/Project1/ClientApp/src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
+//import { NavMenu } from './NavMenu';
+import NavBarSemanticUI from './NavBarSemanticUI';
 
 export class Layout extends Component {
   static displayName = Layout.name;
@@ -8,7 +9,7 @@ export class Layout extends Component {
   render () {
     return (
       <div>
-        <NavMenu />
+        <NavBarSemanticUI />
         <Container>
           {this.props.children}
         </Container>
